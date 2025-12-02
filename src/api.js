c49c199754d6c02828933ee7aefc537419441616
Sandra0625 <sandrapabon678@gmail.com>
2025-12-02 18:41:27 -0500

    Actualización del proyecto

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api' // Ajusta si tu backend corre en otro puerto
+  baseURL: 'http://localhost:4000/api' // Ajusta si tu backend corre en otro puerto
 });
 
 export default api;
